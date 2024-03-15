81a55c81acee7ac249cfb1c616740583eb6a489b
Éleson Silva <elesonsilva9@gmail.com>
2024-03-15 11:48:22 -0300

    Update racas.service.ts
    usando chave de autenticação da api

--- src/app/services/racas.service.ts
+++ src/app/services/racas.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { racas } from '../Models/racas.model';
 @Injectable({
   providedIn: 'root'
 })
 export class RacasService {
+
+  header = new HttpHeaders({
+    'x-api-key':'live_IzZ7UeqzEaR5xx26QoJPsLMSBEqKf68zw2skJpUC6XC88QpvZPbByrGlJWY9ddiN'
+  });
+
   private url = environment.api
 
   constructor(private httpclient:HttpClient) { }
 
   obterRacas(){
-    return this.httpclient.get<racas[]>(this.url + '/v1/images/search?limit=10')
+    return this.httpclient.get<racas[]>(this.url + '/v1/images/search?limit=20',{headers: this.header})
   }
 }
